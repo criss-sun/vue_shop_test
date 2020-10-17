04d2d1a3ce6dce3a7f997f77e4a15a1ff7adb448
criss-sun <248817095@qq.com>
2020-10-17 21:13:30 +0800

    完成了登陆功能

--- src/main.js
+++ src/main.js
@@ -1,6 +1,13 @@
 import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
+import 'assets/css/global.css'
+import 'plugins/element'
+import axios from 'axios'
+
+axios.defaults.baseURL = 'http://127.0.0.1:8888/api/private/v1/'
+
+Vue.prototype.$axios = axios
 
 Vue.config.productionTip = false
 
